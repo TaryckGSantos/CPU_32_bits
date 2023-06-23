3e30c099a235ea246ced27781583cdb4e6a2708d
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2023-06-23 09:26:00 -0300

    Update and_32bits.vhd

--- ULA/AND/and_32bits.vhd
+++ ULA/AND/and_32bits.vhd
@@ -1,1 +1,13 @@
-....
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity and_32bits is
+port ( i_A1 : in std_logic_vector(31 downto 0);
+		 i_A0 : in std_logic_vector(31 downto 0);
+		 o_Q : out std_logic_vector(31 downto 0));
+end and_32bits;
+	
+architecture arch_1 of and_32bits is 
+begin
+		o_Q <= i_A1 and i_A0;
+end arch_1;
