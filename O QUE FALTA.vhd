02194b750121ca4c71a4aba1d86cd63925ab5f1e
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2023-06-23 10:53:44 -0300

    Update O QUE FALTA.vhd

--- O QUE FALTA.vhd
+++ O QUE FALTA.vhd
@@ -1,5 +1,6 @@
 - Fazer um set on less than pra ULA
-
+- Pesquisar como fazer o registerFile da maneira certa
+- 
 
 
 Repositório para usar de parâmetro: https://github.com/dugagjin/MIPS/tree/master
