a5be806221038a56eda8dd8e4dd6fc3cdfd18afb
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2023-06-23 09:16:29 -0300

    Update and rename shift_left_2.vhd to shift_left_32bits.vhd

--- SHIFT LEFT 2/shift_left_32bits.vhd
+++ SHIFT LEFT 2/shift_left_32bits.vhd
@@ -1,12 +1,12 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity shift_left_2 is
+entity shift_left_32bits is
 port ( i_R : in std_logic_vector(31 downto 0);
 		 o_Q : out std_logic_vector(31 downto 0));
-end shift_left_2;
+end shift_left_32bits;
 	
-architecture arch_1 of shift_left_2 is 
+architecture arch_1 of shift_left_32bits is 
 signal w_Q : std_logic_vector(31 downto 0);
 begin
 		w_Q <= i_R(29 downto 0) & "00";
