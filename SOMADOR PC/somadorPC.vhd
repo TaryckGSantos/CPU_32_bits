71d1799300e10a326286c1058064ce71b8d65d17
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2023-06-25 17:17:26 -0300

    Update somadorPC.vhd

--- SOMADOR PC/somadorPC.vhd
+++ SOMADOR PC/somadorPC.vhd
@@ -3,14 +3,14 @@ use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 
 entity somadorPC is
-port (	i_A : in std_logic_vector (31 downto 0);
-			o_S : out std_logic_vector (31 downto 0));
+port (	i_PC : in std_logic_vector (31 downto 0);
+			o_somadorPC : out std_logic_vector (31 downto 0));
 end somadorPC;
 	
 architecture design of somadorPC is
 
 begin
 		
-		o_S <= i_A + 4;
+		o_somadorPC <= i_PC + 4;
 	
 end design;
