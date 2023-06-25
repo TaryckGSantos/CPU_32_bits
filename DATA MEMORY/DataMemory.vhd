5559f2a5b2fae94304f6ab93cde1485813301104
Taryck G. Santos <108833936+TaryckGSantos@users.noreply.github.com>
2023-06-24 22:05:01 -0300

    Update DataMemory.vhd

--- DATA MEMORY/DataMemory.vhd
+++ DATA MEMORY/DataMemory.vhd
@@ -13,11 +13,9 @@ end DataMemory;
 
 architecture arch_1 of DataMemory is
 
-	type RAM_16_x_32 is array(15 downto 0) of std_logic_vector(31 downto 0);
+	type RAM is array(0 to 255) of std_logic_vector(31 downto 0);
 	
-	signal w_Data_Memory : RAM_16_x_32;
-	
-	signal w_ReadData  :  std_logic_vector (31 downto 0);
+	signal w_Data_Memory : RAM := ((others=> (others=>'0')));
 	
 begin
 	
@@ -25,15 +23,15 @@ begin
 	begin
 		if(rising_edge(i_clk)) then
 			if (i_MemWrite = '1') then
-				w_Data_Memory((to_integer(unsigned(i_Address)) - 268500992) / 4) <= i_WriteData;
+				w_Data_Memory((to_integer(unsigned(i_Address(7 downto 0))))) <= i_WriteData;
 				
 			end if;
 			
 			if (i_MemRead = '1') then
-				w_ReadData <= w_Data_Memory((to_integer(unsigned(i_Address)) - 268500992)/4);
+				o_ReadData <= w_Data_Memory((to_integer(unsigned(i_Address(7 downto 0)))));
 				
 			end if;
+			
 		end if;
 	end process;
-		o_ReadData <= w_ReadData;
 end arch_1;
