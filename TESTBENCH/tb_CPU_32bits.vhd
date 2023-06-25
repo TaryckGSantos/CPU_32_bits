c189ae3d41582f6db6ee84c5a3936ea7e9e31468
Cauã Domingos <102312253+cauadomingos72@users.noreply.github.com>
2023-06-25 20:26:20 -0300

    Create tb_CPU_32bits.vhd

---
+++ TESTBENCH/tb_CPU_32bits.vhd
@@ -0,0 +1,32 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity tb_CPU_32bits is
+end tb_CPU_32bits;
+
+architecture arch_1 of tb_CPU_32bits is
+component CPU_32bits is 
+port ( i_clk : in std_logic);
+end component
+
+signal w_clk: std_logic;
+
+begin
+
+	u_DUT:	CPU_32bits port map(i_clk => w_clk);
+
+	process
+	begin
+	
+	w_clk <= 0;
+	wait for 10ns;
+	
+	w_clk <= 1;
+	wait for 10ns;
+	
+	w_clk <= 0;
+	assert false report "Test done." severity note;
+   wait;
+	end process;
+	
+end arch_1;
